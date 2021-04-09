68508f6246b80404edb3fc2e5042209efcf52ab8
php1301 <19520854@gm.uit.edu.vn>
2021-04-10 00:39:37 +0700

    heThongRap and cumRap model added

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,5 @@ export * from './users.model';
 export * from './user-credentials.model';
 export * from './loai-nguoi-dung.model';
 export * from './rap.model';
+export * from './cum-rap.model';
+export * from './he-thong-rap.model';
