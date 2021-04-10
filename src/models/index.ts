f7dd3b6888d799f5f4c45b1aa9bad9cbba9626e4
php1301 <19520854@gm.uit.edu.vn>
2021-04-10 10:32:37 +0700

    LoaiGhe model added

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,3 +4,5 @@ export * from './loai-nguoi-dung.model';
 export * from './rap.model';
 export * from './cum-rap.model';
 export * from './he-thong-rap.model';
+export * from './ghe.model';
+export * from './loai-ghe.model';
---
+++ src/models/loai-ghe.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class LoaiGhe extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  maLoaiGhe?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  tenLoaiGhe: string;
+
+  @property({
+    type: 'string',
+  })
+  moTa?: string;
+  
+  @property({
+    type: 'number',
+    default: 0,
+  })
+  chietKhau?: number;
+
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<LoaiGhe>) {
+    super(data);
+  }
+}
+
+export interface LoaiGheRelations {
+  // describe navigational properties here
+}
+
+export type LoaiGheWithRelations = LoaiGhe & LoaiGheRelations;
