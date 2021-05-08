8baebc64bf7d362bb0988f6b41c7dbdc8db67c0b
php1301 <19520854@gm.uit.edu.vn>
2021-05-08 22:00:15 +0700

    phim-theloai modedl

---
+++ src/models/phim-the-loai.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class PhimTheLoai extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  isbn?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  maPhim: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  maTheLoai: number;
+
+
+  constructor(data?: Partial<PhimTheLoai>) {
+    super(data);
+  }
+}
+
+export interface PhimTheLoaiRelations {
+  // describe navigational properties here
+}
+
+export type PhimTheLoaiWithRelations = PhimTheLoai & PhimTheLoaiRelations;
