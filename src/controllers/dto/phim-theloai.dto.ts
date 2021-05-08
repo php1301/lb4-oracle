9703ceaea58934522d222a2ab17662f119a5e846
php1301 <19520854@gm.uit.edu.vn>
2021-05-08 21:58:34 +0700

    phim-theloai DTO added

---
+++ src/controllers/dto/phim-theloai.dto.ts
@@ -0,0 +1,21 @@
+import {RequestBodyObject} from '@loopback/rest';
+export const theLoaiRequest: Partial<RequestBodyObject> = {
+  description: 'Request cho gan cac the loai vao phim',
+  content: {
+    'application/json': {
+      schema: {
+        properties: {
+          theLoai: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                maTheLoai: {type: 'string'},
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
