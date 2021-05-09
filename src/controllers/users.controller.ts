92884520c6621b86dd0d22358cc79c5d31dc2424
php1301 <19520854@gm.uit.edu.vn>
2021-05-09 15:45:14 +0700

    DoiMatKhau added

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -8,12 +8,14 @@ import {model, property, repository} from '@loopback/repository';
 import {
   get,
   getModelSchemaRef,
+  HttpErrors,
   post,
   requestBody,
+  RequestBodyObject,
   SchemaObject,
 } from '@loopback/rest';
 import {SecurityBindings, securityId, UserProfile} from '@loopback/security';
-import {genSalt, hash} from 'bcryptjs';
+import {genSalt, hash, compare} from 'bcryptjs';
 import {
   Credentials,
   TokenServiceBindings,
@@ -21,6 +23,7 @@ import {
 } from '../components/jwt-authentication';
 import {Users} from '../models';
 import {UsersRepository} from '../repositories';
+import {doiPasswordRequest} from './dto/user.dto';
 
 @model()
 export class NewUserRequest extends Users {
@@ -117,10 +120,74 @@ export class UserController {
     @inject(SecurityBindings.USER)
     currentUserProfile: UserProfile,
   ): Promise<string> {
-    console.log(currentUserProfile)
+    console.log(currentUserProfile);
     return currentUserProfile[securityId];
   }
+  @authenticate('jwt')
+  @post('/change-password', {
+    responses: {
+      '200': {
+        description: 'Message',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: {
+                message: {
+                  type: 'string',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  async doiMatKhau(
+    @inject(SecurityBindings.USER)
+    currentUserProfile: UserProfile,
+    @requestBody(doiPasswordRequest)
+    request: RequestBodyObject,
+  ): Promise<{message: string}> {
+    const invalidCredentialsError = 'Invalid email or password.';
+    console.log(request);
+    try {
+      const user = await this.userRepository.find({
+        where: {
+          email: currentUserProfile.email,
+        },
+      });
+      const passwordMatched = await compare(
+        request.oldPassword,
+        user[0].password,
+      );
+      console.log(passwordMatched);
 
+      if (!passwordMatched) {
+        throw new HttpErrors.Unauthorized(invalidCredentialsError);
+      }
+      const newPassword = await hash(request.newPassword, await genSalt(10));
+
+      await this.userRepository
+        .userCredentials(user[0].taiKhoan)
+        .patch({password: newPassword});
+      return {
+        message: 'Password changed',
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        message: 'Failed to change password',
+      };
+    }
+    // const validate = await hash(request.password, await genSalt(10));
+    // const savedUser = await this.userRepository.create(newUserRequest);
+    // await this.userRepository
+    //   .userCredentials(savedUser.taiKhoan)
+    //   .create({password});
+
+    // return savedUser;
+  }
   @post('/signup', {
     responses: {
       '200': {
