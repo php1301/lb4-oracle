89518993b405b87789ddc10113cc470240197c59
php1301 <19520854@gm.uit.edu.vn>
2021-05-16 19:15:58 +0700

    LichChieu repository relation modified

--- src/repositories/lich-chieu.repository.ts
+++ src/repositories/lich-chieu.repository.ts
@@ -1,10 +1,12 @@
 import {inject, Getter} from '@loopback/core';
-import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
+import {DefaultCrudRepository, repository, BelongsToAccessor, HasManyThroughRepositoryFactory} from '@loopback/repository';
 import {DbDataSource} from '../datasources';
-import {LichChieu, LichChieuRelations, Rap, HeThongRap, CumRap} from '../models';
+import {LichChieu, LichChieuRelations, Rap, HeThongRap, CumRap, Ghe, GheDaDat} from '../models';
 import {RapRepository} from './rap.repository';
 import {HeThongRapRepository} from './he-thong-rap.repository';
 import {CumRapRepository} from './cum-rap.repository';
+import {GheDaDatRepository} from './ghe-da-dat.repository';
+import {GheRepository} from './ghe.repository';
 
 export class LichChieuRepository extends DefaultCrudRepository<
   LichChieu,
@@ -18,10 +20,17 @@ export class LichChieuRepository extends DefaultCrudRepository<
 
   public readonly lichChieuCumRap: BelongsToAccessor<CumRap, typeof LichChieu.prototype.maLichChieu>;
 
+  public readonly gheDaDat: HasManyThroughRepositoryFactory<Ghe, typeof Ghe.prototype.maGhe,
+          GheDaDat,
+          typeof LichChieu.prototype.maLichChieu
+        >;
+
   constructor(
-    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('RapRepository') protected rapRepositoryGetter: Getter<RapRepository>, @repository.getter('HeThongRapRepository') protected heThongRapRepositoryGetter: Getter<HeThongRapRepository>, @repository.getter('CumRapRepository') protected cumRapRepositoryGetter: Getter<CumRapRepository>,
+    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('RapRepository') protected rapRepositoryGetter: Getter<RapRepository>, @repository.getter('HeThongRapRepository') protected heThongRapRepositoryGetter: Getter<HeThongRapRepository>, @repository.getter('CumRapRepository') protected cumRapRepositoryGetter: Getter<CumRapRepository>, @repository.getter('GheDaDatRepository') protected gheDaDatRepositoryGetter: Getter<GheDaDatRepository>, @repository.getter('GheRepository') protected gheRepositoryGetter: Getter<GheRepository>,
   ) {
     super(LichChieu, dataSource);
+    this.gheDaDat = this.createHasManyThroughRepositoryFactoryFor('gheDaDat', gheRepositoryGetter, gheDaDatRepositoryGetter,);
+    this.registerInclusionResolver('gheDaDat', this.gheDaDat.inclusionResolver);
     this.lichChieuCumRap = this.createBelongsToAccessorFor('lichChieuCumRap', cumRapRepositoryGetter,);
     this.registerInclusionResolver('lichChieuCumRap', this.lichChieuCumRap.inclusionResolver);
     this.lichChieuHeThongRap = this.createBelongsToAccessorFor('lichChieuHeThongRap', heThongRapRepositoryGetter,);
