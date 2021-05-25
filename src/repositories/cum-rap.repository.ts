37855c85a79d86881d8d4f5b92bd64ae0d9ecec0
php1301 <19520854@gm.uit.edu.vn>
2021-05-25 17:34:44 +0700

    Relations added for CumRapRepository

--- src/repositories/cum-rap.repository.ts
+++ src/repositories/cum-rap.repository.ts
@@ -2,11 +2,12 @@ import {inject, Getter} from '@loopback/core';
 import {
   DefaultCrudRepository,
   repository,
-  BelongsToAccessor,
-} from '@loopback/repository';
+  BelongsToAccessor, HasManyRepositoryFactory} from '@loopback/repository';
 import {DbDataSource} from '../datasources';
-import {CumRap, CumRapRelations, HeThongRap} from '../models';
+import {CumRap, CumRapRelations, HeThongRap, LichChieu, Rap} from '../models';
 import {HeThongRapRepository} from './he-thong-rap.repository';
+import {LichChieuRepository} from './lich-chieu.repository';
+import {RapRepository} from './rap.repository';
 
 export class CumRapRepository extends DefaultCrudRepository<
   CumRap,
@@ -18,12 +19,20 @@ export class CumRapRepository extends DefaultCrudRepository<
     typeof CumRap.prototype.maCumRap
   >;
 
+  public readonly lichChieuCumRap: HasManyRepositoryFactory<LichChieu, typeof CumRap.prototype.maCumRap>;
+
+  public readonly rapCuaCumRap: HasManyRepositoryFactory<Rap, typeof CumRap.prototype.maCumRap>;
+
   constructor(
     @inject('datasources.db') dataSource: DbDataSource,
     @repository.getter('HeThongRapRepository')
-    protected heThongRapRepositoryGetter: Getter<HeThongRapRepository>,
+    protected heThongRapRepositoryGetter: Getter<HeThongRapRepository>, @repository.getter('LichChieuRepository') protected lichChieuRepositoryGetter: Getter<LichChieuRepository>, @repository.getter('RapRepository') protected rapRepositoryGetter: Getter<RapRepository>,
   ) {
     super(CumRap, dataSource);
+    this.rapCuaCumRap = this.createHasManyRepositoryFactoryFor('rapCuaCumRap', rapRepositoryGetter,);
+    this.registerInclusionResolver('rapCuaCumRap', this.rapCuaCumRap.inclusionResolver);
+    this.lichChieuCumRap = this.createHasManyRepositoryFactoryFor('lichChieuCumRap', lichChieuRepositoryGetter,);
+    this.registerInclusionResolver('lichChieuCumRap', this.lichChieuCumRap.inclusionResolver);
     this.cumRapHeThongRap = this.createBelongsToAccessorFor(
       'cumrap_hethongrap',
       heThongRapRepositoryGetter,
