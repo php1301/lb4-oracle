b90690b456e59f84db3ead730f0715bb2eccbdc7
php1301 <19520854@gm.uit.edu.vn>
2021-05-25 17:34:01 +0700

    HeThongRap repository added

--- src/repositories/he-thong-rap.repository.ts
+++ src/repositories/he-thong-rap.repository.ts
@@ -1,16 +1,22 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory} from '@loopback/repository';
 import {DbDataSource} from '../datasources';
-import {HeThongRap, HeThongRapRelations} from '../models';
+import {HeThongRap, HeThongRapRelations, CumRap} from '../models';
+import {CumRapRepository} from './cum-rap.repository';
 
 export class HeThongRapRepository extends DefaultCrudRepository<
   HeThongRap,
   typeof HeThongRap.prototype.maHeThongRap,
   HeThongRapRelations
 > {
+
+  public readonly cacCumRapHeThongRap: HasManyRepositoryFactory<CumRap, typeof HeThongRap.prototype.maHeThongRap>;
+
   constructor(
-    @inject('datasources.db') dataSource: DbDataSource,
+    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('CumRapRepository') protected cumRapRepositoryGetter: Getter<CumRapRepository>,
   ) {
     super(HeThongRap, dataSource);
+    this.cacCumRapHeThongRap = this.createHasManyRepositoryFactoryFor('cacCumRapHeThongRap', cumRapRepositoryGetter,);
+    this.registerInclusionResolver('cacCumRapHeThongRap', this.cacCumRapHeThongRap.inclusionResolver);
   }
 }
