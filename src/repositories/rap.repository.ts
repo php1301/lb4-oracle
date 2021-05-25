39beb72f26afef7f4c27a04e88b73faadaab9e70
php1301 <19520854@gm.uit.edu.vn>
2021-05-25 17:32:40 +0700

    Ghe and LichChieu relation added for Rap

--- src/repositories/rap.repository.ts
+++ src/repositories/rap.repository.ts
@@ -2,11 +2,12 @@ import {inject, Getter} from '@loopback/core';
 import {
   DefaultCrudRepository,
   repository,
-  BelongsToAccessor,
-} from '@loopback/repository';
+  BelongsToAccessor, HasManyRepositoryFactory} from '@loopback/repository';
 import {DbDataSource} from '../datasources';
-import {Rap, RapRelations, CumRap} from '../models';
+import {Rap, RapRelations, CumRap, Ghe, LichChieu} from '../models';
 import {CumRapRepository} from './cum-rap.repository';
+import {GheRepository} from './ghe.repository';
+import {LichChieuRepository} from './lich-chieu.repository';
 
 export class RapRepository extends DefaultCrudRepository<
   Rap,
@@ -18,12 +19,20 @@ export class RapRepository extends DefaultCrudRepository<
     typeof Rap.prototype.maRap
   >;
 
+  public readonly rapCuaGhe: HasManyRepositoryFactory<Ghe, typeof Rap.prototype.maRap>;
+
+  public readonly lichChieuCuaRap: HasManyRepositoryFactory<LichChieu, typeof Rap.prototype.maRap>;
+
   constructor(
     @inject('datasources.db') dataSource: DbDataSource,
     @repository.getter('CumRapRepository')
-    protected cumRapRepositoryGetter: Getter<CumRapRepository>,
+    protected cumRapRepositoryGetter: Getter<CumRapRepository>, @repository.getter('GheRepository') protected gheRepositoryGetter: Getter<GheRepository>, @repository.getter('LichChieuRepository') protected lichChieuRepositoryGetter: Getter<LichChieuRepository>,
   ) {
     super(Rap, dataSource);
+    this.lichChieuCuaRap = this.createHasManyRepositoryFactoryFor('lichChieuCuaRap', lichChieuRepositoryGetter,);
+    this.registerInclusionResolver('lichChieuCuaRap', this.lichChieuCuaRap.inclusionResolver);
+    this.rapCuaGhe = this.createHasManyRepositoryFactoryFor('rapCuaGhe', gheRepositoryGetter,);
+    this.registerInclusionResolver('rapCuaGhe', this.rapCuaGhe.inclusionResolver);
     this.rapCumRap = this.createBelongsToAccessorFor(
       'rap_cumRap',
       cumRapRepositoryGetter,
